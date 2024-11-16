141cf006970ce798f2aa1656fdcd3311f1bafeb8
Alan James <10474378+alanjohnjames@users.noreply.github.com>
2024-11-16 22:28:31 +0000

    ADD: `rules.py`

---
+++ rules.py
@@ -0,0 +1,23 @@
+"""
+Simple rules based engine
+
+https://dev.to/fractalis/how-to-write-a-basic-rule-engine-in-python-3eik
+
+"""
+
+from typing import Any
+
+class Fact:
+    def __init__(self, **kwargs: Any):
+        self.__dict__.update(kwargs)
+
+def fact_test():
+    person_fact = Fact(name="John Brown",
+                    age="35",
+                    occuptation="Software Developer")
+
+    person_fact.age # Returns 35
+
+
+
+
