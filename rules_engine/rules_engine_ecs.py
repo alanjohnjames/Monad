ff20c11c81bc60047faf82b415027ce86414c108
Alan James <10474378+alanjohnjames@users.noreply.github.com>
2024-12-18 00:38:01 +0000

    CHANGE: separate RulsSystem into ConditionSystem and ActionSystem

--- rules_engine/rules_engine_ecs.py
+++ rules_engine/rules_engine_ecs.py
@@ -21,17 +21,26 @@ class Entity:
     def get_component(self, component_type):
         return self.components.get(component_type)
 
-class RuleSystem:
-    def __init__(self, condition, action):
+class ConditionSystem:
+    def __init__(self, condition):
         self.condition = condition
-        self.action = action
+        self.satisfied_entities = []
 
     def process(self, entities):
+        self.satisfied_entities = []
         for entity in entities:
             data = entity.get_component(DataComponent)
             if data and self.condition(data.value):
-                self.action(entity)
-                entities.remove(entity)
+                self.satisfied_entities.append(entity)
+
+class ActionSystem:
+    def __init__(self, action):
+        self.action = action
+
+    def process(self, entities, satisfied_entities):
+        for entity in satisfied_entities:
+            self.action(entity)
+            entities.remove(entity)
 
 class World:
     def __init__(self):
@@ -46,7 +55,10 @@ class World:
 
     def process(self):
         for system in self.systems:
-            system.process(self.entities)
+            if isinstance(system, ConditionSystem):
+                system.process(self.entities)
+            elif isinstance(system, ActionSystem):
+                system.process(self.entities, system.satisfied_entities)
 
 if __name__ == "__main__":
 
@@ -64,8 +76,19 @@ if __name__ == "__main__":
     rule2_action = lambda entity: print(f"Entity {entity.id}: Rule 2 is satisfied.")
     rule3_action = lambda entity: print(f"Entity {entity.id}: Rule 3 is satisfied.")
 
-    world.add_system(RuleSystem(rule1_condition, rule1_action))
-    world.add_system(RuleSystem(rule2_condition, rule2_action))
-    world.add_system(RuleSystem(rule3_condition, rule3_action))
+    condition_system1 = ConditionSystem(rule1_condition)
+    condition_system2 = ConditionSystem(rule2_condition)
+    condition_system3 = ConditionSystem(rule3_condition)
+
+    action_system1 = ActionSystem(rule1_action)
+    action_system2 = ActionSystem(rule2_action)
+    action_system3 = ActionSystem(rule3_action)
+
+    world.add_system(condition_system1)
+    world.add_system(condition_system2)
+    world.add_system(condition_system3)
+    world.add_system(action_system1)
+    world.add_system(action_system2)
+    world.add_system(action_system3)
 
     world.process()
